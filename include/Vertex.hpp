da6d13a32e6df90dcab98377da901e14e10d75ff
SirHall <sirhallstein@gmail.com>
2019-10-25 20:55:45 +1000

    Added early WIP support for UV's to Vertex

--- include/Vertex.hpp
+++ include/Vertex.hpp
@@ -10,10 +10,12 @@ struct Vertex
 {
 public:
     Vertex();
-    Vertex(GLfloat x, GLfloat y, GLfloat z);
+    Vertex(GLfloat pos_x, GLfloat pos_y, GLfloat pos_z, GLfloat uv_x,
+           GLfloat uv_y);
     ~Vertex();
     static GLuint GenerateAttributes();
     GLfloat       position[3] = {0.0f, 0.0f, 0.0f};
+    GLfloat       uv[2]       = {0.0f, 0.0f};
 };
 
 #endif
--- src/Vertex.cpp
+++ src/Vertex.cpp
@@ -7,11 +7,15 @@
 
 Vertex::Vertex() {}
 
-Vertex::Vertex(GLfloat x, GLfloat y, GLfloat z)
+Vertex::Vertex(GLfloat pos_x, GLfloat pos_y, GLfloat pos_z, GLfloat uv_x = 0.0f,
+               GLfloat uv_y = 0.0f)
 {
-    position[0] = x;
-    position[1] = y;
-    position[2] = z;
+    position[0] = pos_x;
+    position[1] = pos_y;
+    position[2] = pos_z;
+
+    uv[0] = uv_x;
+    uv[1] = uv_y;
 }
 
 Vertex::~Vertex() {}
@@ -28,9 +32,15 @@ GLuint Vertex::GenerateAttributes()
 
     GLuint currentIndex = 0;
 
+    // Model position attribute
     GLCall(glVertexAttribPointer(
         currentIndex++, sizeof(position) / sizeof(GLfloat), GL_FLOAT, GL_FALSE,
         sizeof(Vertex), reinterpret_cast<void *>(offsetof(Vertex, position))));
+
+    // UV Coordinate Attribute
+    GLCall(glVertexAttribPointer(
+        currentIndex++, sizeof(uv) / sizeof(GLfloat), GL_FLOAT, GL_FALSE,
+        sizeof(Vertex), reinterpret_cast<void *>(offsetof(Vertex, uv))));
 
     return vao;
 }
